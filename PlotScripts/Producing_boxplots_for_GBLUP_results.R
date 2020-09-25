3546ab76b1c6cc36e2473637ffdbbb6f7fc7fd3b
Cathrine Kiel Skovbjerg <42338141+cks2903@users.noreply.github.com>
2020-09-25 12:41:01 +0200

    Update Producing_boxplots_for_GBLUP_results.R

--- PlotScripts/Producing_boxplots_for_GBLUP_results.R
+++ PlotScripts/Producing_boxplots_for_GBLUP_results.R
@@ -1,6 +1,7 @@
 library("ggplot2")
 library("wesanderson")
 
+setwd("/Volumes/NAT_MBG-PMg/Cathrine/Nchain/Genomic_prediction_yield_July2020/V2_LessHarsh_SaraQualityFilter/Article_GP_gpd")
 
 # accuracies and force zero
 # Boxplots to compare prediction of different iSize corrected traits
@@ -17,12 +18,12 @@ Prediction_accuracies$Accuracy[forcetobezero]=as.numeric(0)
 #png(file="PredictionAccuraciesOfDifferent_gpdTraits_20200911.png",width=20,height=10,units="cm",res=300)
 ggplot(Prediction_accuracies, aes(x=Trait, y=Accuracy,fill=Trait)) + 
   geom_boxplot(aes(fill = Trait))+
-  geom_point(aes(fill = Trait), size = 1, shape = 21, position = position_jitterdodge()) +
+  geom_point(aes(fill = Trait), size = 2, shape = 21, position = position_jitterdodge()) +
   scale_fill_manual(values=wes_palette("Rushmore1")[c(2,4,3)]) +
   ylim(c(min(Prediction_accuracies$Accuracy),0.615)) +
   theme_classic() +theme(axis.text.x = element_text(size = 10, angle = 90,hjust=1),axis.text.y = element_text(size = 10), 
                          axis.title=element_text(size=15))
-ggsave("PredictionAccuraciesOfDifferent_gpdTraits_20200924.pdf", width =20, height = 10, units = "cm")
+ggsave("/Volumes/NAT_MBG-PMg/Cathrine/Nchain/Genomic_prediction_yield_July2020/V2_LessHarsh_SaraQualityFilter/Article_GP_gpd/PredictionAccuraciesOfDifferent_gpdTraits_20200924.pdf", width =30, height = 20, units = "cm",useDingbats=FALSE)
 #dev.off()
 
 
@@ -30,12 +31,12 @@ ggsave("PredictionAccuraciesOfDifferent_gpdTraits_20200924.pdf", width =20, heig
 #png(file="PredictionAccuraciesOfDifferent_gpdTraits_20200911_realvalues.png",width=20,height=10,units="cm",res=300)
 ggplot(Prediction_accuracies_true, aes(x=Trait, y=Accuracy,fill=Trait)) + 
   geom_boxplot(aes(fill = Trait))+
-  geom_point(aes(fill = Trait), size = 1, shape = 21, position = position_jitterdodge()) +
+  geom_point(aes(fill = Trait), size = 2, shape = 21, position = position_jitterdodge()) +
   scale_fill_manual(values=wes_palette("Rushmore1")[c(2,4,3)]) +
   ylim(c(min(Prediction_accuracies_true$Accuracy),0.615)) +
   theme_classic() +theme(axis.text.x = element_text(size = 10, angle = 90,hjust=1),axis.text.y = element_text(size = 10), 
                          axis.title=element_text(size=15))
-ggsave("PredictionAccuraciesOfDifferent_gpdTraits_20200924_realvalues.pdf", width =20, height = 10, units = "cm")
+ggsave("/Volumes/NAT_MBG-PMg/Cathrine/Nchain/Genomic_prediction_yield_July2020/V2_LessHarsh_SaraQualityFilter/Article_GP_gpd/PredictionAccuraciesOfDifferent_gpdTraits_20200924_realvalues.pdf", width =30, height = 20, units = "cm",useDingbats=FALSE)
 #dev.off()
 
 # Figure 4 in manuscript
@@ -43,11 +44,11 @@ ggsave("PredictionAccuraciesOfDifferent_gpdTraits_20200924_realvalues.pdf", widt
 
 ggplot(Prediction_accuracies_true, aes(x=Trait, y=Correlation,fill=Trait)) + 
   geom_boxplot(aes(fill = Trait))+
-  geom_point(aes(fill = Trait), size = 1, shape = 21, position = position_jitterdodge()) +
+  geom_point(aes(fill = Trait), size = 2, shape = 21, position = position_jitterdodge()) +
   scale_fill_manual(values=wes_palette("Rushmore1")[c(2,4,3)]) +
   ylim(c(min(Prediction_accuracies_true$Correlation),max(Prediction_accuracies_true$Correlation))) +
   theme_classic() +theme(axis.text.x = element_text(size = 10, angle = 90,hjust=1),axis.text.y = element_text(size = 10), 
                          axis.title=element_text(size=15))
-ggsave("PredictionCorrelationsOfDifferent_gpdTraits_20200924.pdf", width =20, height = 10, units = "cm")
+ggsave("/Volumes/NAT_MBG-PMg/Cathrine/Nchain/Genomic_prediction_yield_July2020/V2_LessHarsh_SaraQualityFilter/Article_GP_gpd/PredictionCorrelationsOfDifferent_gpdTraits_20200924.pdf", width =30, height = 20, units = "cm",useDingbats=FALSE)
 
 
